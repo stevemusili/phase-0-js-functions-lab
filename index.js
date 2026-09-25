cc5cef1c949727a36d60afc0ebb6a963aebb0cef
stevemusili <musilisteve223@gmail.com>
2026-09-25 15:08:38 +0300

    create function 1:calculateTax

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+calculateTax(5000)
 
-
+console.log("Tax value;", calculateTax(5000))
 
 
 // This is required for the test to function properly  
