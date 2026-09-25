d0691409021991de07a6ad50ddb33eb66ca93424
stevemusili <musilisteve223@gmail.com>
2026-09-25 15:22:50 +0300

    create function 2: convertToUpperCase

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// Function 1: calculateTax
 function calculateTax(amount) {
     return amount * 0.10;
 }
 
 calculateTax(5000)
 
-console.log("Tax value;", calculateTax(5000))
+console.log(calculateTax(5000))
+
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+convertToUpperCase("steve")
+console.log(convertToUpperCase("steve"))
+
 
 
 // This is required for the test to function properly  
